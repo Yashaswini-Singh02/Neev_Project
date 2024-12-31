bae425f2c0939dc77362ee97c10d66f171761791
Yashaswini Singh Shaktawat <yashaswinisinghshaktawat@gmail.com>
2024-12-31 21:22:23 +0530

    fix:ui

--- client/components/features/features.tsx
+++ client/components/features/features.tsx
@@ -3,7 +3,7 @@ import { FeatureCard } from "./feature-card";
 export const Features: React.FC = () => {
   return (
     <section className="py-10 px-32 w-full h-screen">
-      <h1 className="text-3xl font-bold mb-8">Features</h1>
+      <h1 className="text-3xl font-bold mb-6">Features</h1>
       <FeatureCard />
     </section>
   );
